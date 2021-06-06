c8afcd6c8403f6f0ce8090abf13583ce2cce2ccf
Fridious <philippfriedhoff@outlook.de>
2021-06-06 20:47:03 +0200

    Updated api version

--- mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/McNativeLauncher.java
+++ mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/McNativeLauncher.java
@@ -56,6 +56,7 @@ import org.mcnative.runtime.api.player.tablist.Tablist;
 import org.mcnative.runtime.api.player.tablist.TablistEntry;
 import org.mcnative.runtime.api.player.tablist.TablistFormatter;
 import org.mcnative.runtime.api.player.tablist.TablistOverviewFormatter;
+import org.mcnative.runtime.api.service.inventory.gui.implemen.GuiListener;
 import org.mcnative.runtime.api.serviceprovider.placeholder.PlaceholderProvider;
 import org.mcnative.runtime.api.text.Text;
 import org.mcnative.runtime.api.text.components.MessageComponent;
@@ -224,6 +225,8 @@ public class McNativeLauncher implements Listener {
 
         McNative.getInstance().getScheduler().createTask(ObjectOwner.SYSTEM).delay(2,TimeUnit.SECONDS)
                 .execute(() -> eventBus.callEvent(LocalServiceStartupEvent.class,new DefaultLocalServiceStartupEvent()));
+
+        eventBus.subscribe(McNative.getInstance(), new GuiListener());
     }
 
     public static void shutdown(){
