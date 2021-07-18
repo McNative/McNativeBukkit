0b065f1af8980cd3ad5803499eb3db7ee4311fa8
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-07-18 14:58:13 +0200

    Updated versions

--- mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/commands/GuiOpenCommand.java
+++ mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/commands/GuiOpenCommand.java
@@ -1,25 +1,39 @@
 package org.mcnative.runtime.bukkit.commands;
 
+import net.pretronic.libraries.command.Completable;
 import net.pretronic.libraries.command.command.BasicCommand;
 import net.pretronic.libraries.command.command.configuration.CommandConfiguration;
 import net.pretronic.libraries.command.sender.CommandSender;
+import net.pretronic.libraries.event.injection.annotations.Inject;
+import net.pretronic.libraries.message.bml.variable.VariableSet;
+import net.pretronic.libraries.utility.Iterators;
 import net.pretronic.libraries.utility.interfaces.ObjectOwner;
 import org.mcnative.runtime.api.McNative;
 import org.mcnative.runtime.api.player.ConnectedMinecraftPlayer;
 import org.mcnative.runtime.api.service.inventory.gui.Gui;
 import org.mcnative.runtime.api.service.inventory.gui.GuiManager;
+import org.mcnative.runtime.api.service.inventory.gui.Page;
+import org.mcnative.runtime.common.Messages;
 
-public class GuiOpenCommand extends BasicCommand {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+public class GuiOpenCommand extends BasicCommand implements Completable {
+
+    private final GuiManager guiManager;
 
     public GuiOpenCommand(ObjectOwner owner) {
         super(owner, CommandConfiguration.name("gui"));
+        this.guiManager = McNative.getInstance().getRegistry().getService(GuiManager.class);
     }
 
     @Override
     public void execute(CommandSender sender, String[] arguments) {
         if(arguments.length == 0){
-            //@Todo error
+            sender.sendMessage(Messages.COMMAND_MCNATIVE_GUI_LIST, VariableSet.create()
+                    .addDescribed("guis",guiManager.getGuis()));
             return;
         }
         String guiName = arguments[0];
@@ -28,14 +42,29 @@ public class GuiOpenCommand extends BasicCommand {
             page = arguments[1];
         }
 
-        GuiManager manager = McNative.getInstance().getRegistry().getService(GuiManager.class);
-        Gui<?> gui = manager.getGui(guiName);
+        Gui<?> gui = guiManager.getGui(guiName);
+
         if(gui == null){
-            sender.sendMessage("Gui not found");
+            sender.sendMessage(Messages.COMMAND_MCNATIVE_GUI_NOT_FOUND, VariableSet.create()
+                    .addDescribed("gui",arguments[0]));
             return;
         }
 
         gui.open((ConnectedMinecraftPlayer) sender,page);
 
+    }
+
+    @Override
+    public Collection<String> complete(CommandSender commandSender, String[] arguments) {
+        if(arguments.length == 0){
+            return Iterators.map(this.guiManager.getGuis(), Gui::getName);
+        }else if(arguments.length == 1){
+            return Iterators.map(this.guiManager.getGuis(), Gui::getName, gui -> gui.getName().toLowerCase().startsWith(arguments[0]));
+        }else if(arguments.length == 2) {
+            Gui<?> gui = guiManager.getGui(arguments[0]);
+            if(gui == null) return Collections.emptyList();
+            else return Iterators.map(gui.getPages(), Page::getName, page -> page.getName().toLowerCase().startsWith(arguments[1]));
+        }
+        return Collections.emptyList();
     }
 }
--- mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/utils/BukkitReflectionUtil.java
+++ mcnative-bukkit/src/main/java/org/mcnative/runtime/bukkit/utils/BukkitReflectionUtil.java
@@ -57,7 +57,7 @@ public class BukkitReflectionUtil {
         protocolVersionsByServerVersion.put("1_16_R1",MinecraftProtocolVersion.JE_1_16_1);
         protocolVersionsByServerVersion.put("1_16_R2",MinecraftProtocolVersion.JE_1_16_3);
         protocolVersionsByServerVersion.put("1_16_R3",MinecraftProtocolVersion.JE_1_16_4);
-        protocolVersionsByServerVersion.put("1_17_R1",MinecraftProtocolVersion.JE_1_17);
+        protocolVersionsByServerVersion.put("1_17_R1",MinecraftProtocolVersion.JE_1_17_1);
 
         try {
             Class.forName("net.minecraft.network.NetworkManager");
